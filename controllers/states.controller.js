2261b26f927f6f1a386fba1e9b63f0cb015c098f
sbell9 <bell137@gmail.com>
2018-04-22 16:37:50 -0500

    committhis

--- controllers/states.controller.js
+++ controllers/states.controller.js
@@ -1,18 +1,5 @@
 let db = require('../db');
 
-// class StatesController {
-
-//     getAllStates = async (req, res) => {
-//         try {
-//             let states = await db.many(`select * from states`);
-//             res.json(states);
-//         } catch (err) {
-//             res.status(404).send({err: err.message});
-//         }
-//     };
-// }
-
-// export default new StatesController();
 let StatesController = {
     getAllStates: async function (req, res) {
         try {
